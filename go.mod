7938966277db668ea286f002913bc7f293a15431
Jeroen van Erp <jeroen@hierynomus.com>
2021-02-17 23:42:32 +0100

    Fix lint

--- go.mod
+++ go.mod
@@ -7,5 +7,4 @@ require (
 	github.com/rs/zerolog v1.20.0
 	github.com/spf13/cobra v1.1.3
 	github.com/stretchr/testify v1.7.0
-
 )
--- pkg/kube/kube_test.go
+++ pkg/kube/kube_test.go
@@ -5,7 +5,7 @@ import (
 	"os"
 	"testing"
 
-	"github.com/tj/assert"
+	"github.com/stretchr/testify/assert"
 )
 
 func TestShouldFindDotKubeInCurrentPath(t *testing.T) {
